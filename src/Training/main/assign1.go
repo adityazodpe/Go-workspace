08d28c2a3dc65f27dfc2f3908f2ec12424c65171
Aditya Zodpe <aditya.zodpe@gmail.com>
2020-12-11 17:41:41 -0600

    updated assign1 file

--- src/Training/main/assign1.go
+++ src/Training/main/assign1.go
@@ -7,10 +7,12 @@ import (
 )
 
 func main() {
+	//Assignment 1
 	fmt.Println("Assignment 1:\n")
 	a := 14.65478
 	fmt.Printf("%.2f\n", a)
 
+	//Assignment 2
 	fmt.Println("\nAssignment 2:\n")
 	input := "32"
 	n, err := strconv.Atoi(input)
@@ -24,12 +26,14 @@ func main() {
 	fmt.Printf("Before converting. Value=%v, Type=%T\n", input2, input2)
 	fmt.Printf("After converting. Value=%v, Type=%T\n\n", n2, n2)
 
+	//Assignment 3
 	fmt.Println("Assignment 3:\n")
 	input3 := "go-lang-training"
 	output := strings.Title(input3)
 	fmt.Println("Input: ", input3)
 	fmt.Println("Output: ", output)
 
+	//Assignment 4
 	fmt.Println("\nAssignment 4:\n")
 	const (
 		x = 6
@@ -38,6 +42,7 @@ func main() {
 	)
 	fmt.Println("x=", x, "y=", y, "z=", z)
 
+	//Assignment 5
 	fmt.Println("\nAssignment 5:\n")
 
 	a5 := 20
@@ -48,6 +53,7 @@ func main() {
 	*p = 40
 	fmt.Println("a(after)= ", a5)
 
+	//Assignment 6
 	fmt.Println("\nAssignment 6:\n")
 	str1 := "1Ax3 7y Bk"
 	res := strings.Count(str1, " ")
